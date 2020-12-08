50fab404b7eeebf1d7346a533ff4e08d7c9d4c56
Łukasz Marek <marek.lucas@gmail.com>
2020-12-08 19:09:13 +0100

    code cleanup

--- src/main/kotlin/day8/Runner.kt
+++ src/main/kotlin/day8/Runner.kt
@@ -22,30 +22,40 @@ fun main() {
 }
 
 fun findCorrectProgramOutput(program: Program): InterpreterState {
-    val indexesToCheck = program.lines.withIndex().filter {
-        when (it.value) {
-            is NopInstruction -> true
-            is JmpInstruction -> true
-            else -> false
-        }
-    }.map { it.index }
+    val indexesToCheck = program.possiblyCorruptedIndexes()
     val interpreter = InterpreterImpl()
+
     val lastInstruction = ProgramLine(program.lines.lastIndex, program.lines.last())
+    val isCorrect = { state: InterpreterState -> state.executedLines.last() == lastInstruction }
+
     for (indexToFlip in indexesToCheck) {
-        val flipped = flipInstruction(program.lines[indexToFlip]!!)
-        val newProgram =
-            program.lines.mapIndexed { index, instruction -> if (index == indexToFlip) flipped else instruction }
-        val result = interpreter.run(Program(newProgram))
-        if (result.executedLines.last() == lastInstruction) {
+        val flipped = program.lines[indexToFlip].flip()
+        val newProgram = program.replaceInstruction(indexToFlip, flipped)
+        val result = interpreter.run(newProgram)
+        if (isCorrect(result)) {
             return result
         }
     }
     throw IllegalStateException("Failed to find correct program output!")
 }
 
-private fun flipInstruction(instruction: ExecutableInstruction): ExecutableInstruction =
-    when (instruction) {
-        is NopInstruction -> JmpInstruction(instruction.arg)
-        is JmpInstruction -> NopInstruction(instruction.jumpBy)
-        else -> throw IllegalArgumentException("Cannot flip $instruction")
+private fun Program.possiblyCorruptedIndexes(): Set<Int> = this.lines.withIndex().filter {
+    when (it.value) {
+        is NopInstruction -> true
+        is JmpInstruction -> true
+        else -> false
     }
+}.map { it.index }.toSet()
+
+private fun ExecutableInstruction.flip(): ExecutableInstruction =
+    when (this) {
+        is NopInstruction -> JmpInstruction(this.arg)
+        is JmpInstruction -> NopInstruction(this.jumpBy)
+        else -> throw IllegalArgumentException("Cannot flip $this")
+    }
+
+private fun Program.replaceInstruction(indexToReplace: Int, replacement: ExecutableInstruction): Program {
+    val newProgram =
+        this.lines.mapIndexed { index, instruction -> if (indexToReplace == index) replacement else instruction }
+    return Program(newProgram)
+}
