7c7a5f5ef26bcdb92b0653e00850111df392e562
Łukasz Marek <marek.lucas@gmail.com>
2020-12-15 20:44:30 +0100

    day15 optimized for part2

--- src/main/kotlin/day15/ElvishGame.kt
+++ src/main/kotlin/day15/ElvishGame.kt
@@ -1,20 +1,20 @@
 package day15
 
 fun play(initialNumbers: List<Int>, rounds: Int): Int {
-    val spokenSoFar = initialNumbers.toMutableList()
-
-    for (round in (initialNumbers.size + 1)..rounds) {
-        val previouslySpoken = spokenSoFar.last()
-        val previousRound = round - 1
-        val lastTimeSpoken = spokenSoFar.dropLast(1).lastIndexOf(previouslySpoken)
-        val spokenNow = if (lastTimeSpoken > -1 && lastTimeSpoken < spokenSoFar.lastIndex) {
-            previousRound - (lastTimeSpoken + 1)
-        } else {
-            0
-        }
-        spokenSoFar.add(spokenNow)
+    val numberToLastSpoken = mutableMapOf<Int, Int>()
+    for ((index, number) in initialNumbers.withIndex().toList().dropLast(1)) {
+        numberToLastSpoken[number] = index + 1
     }
 
-    return spokenSoFar.last()
+    var previouslySpoken = initialNumbers.last()
+    for (round in (initialNumbers.size + 1)..rounds) {
+        val previousRound = round - 1
+        val lastTimeSpoken = numberToLastSpoken[previouslySpoken]
+        val spokenNow = lastTimeSpoken?.let { previousRound - it } ?: 0
+        numberToLastSpoken[previouslySpoken] = previousRound
+        previouslySpoken = spokenNow
+    }
+
+    return previouslySpoken
 }
 
--- src/main/kotlin/day15/Runner.kt
+++ src/main/kotlin/day15/Runner.kt
@@ -4,4 +4,7 @@ fun main() {
     val initialNumbers = listOf(13, 16, 0, 12, 15, 1)
     val rounds = 2020
     println("${rounds}th number is ${play(initialNumbers, rounds)}")
+    val rounds2 = 30000000
+    println("${rounds2}th number is ${play(initialNumbers, rounds2)}")
+
 }
