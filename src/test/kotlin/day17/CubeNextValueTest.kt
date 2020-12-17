00cc46e43f99aa1f0498a994da16cddea1c45ac0
Łukasz Marek <marek.lucas@gmail.com>
2020-12-17 22:45:00 +0100

    day17 tests in progress

--- src/test/kotlin/day17/CubeNextValueTest.kt
+++ src/test/kotlin/day17/CubeNextValueTest.kt
@@ -32,7 +32,7 @@ class CubeNextValueTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = [0, 1, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25])
+    @ValueSource(ints = [0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25])
     fun `next should not be alive if not 2 neighbours and is not alive now`(aliveNeighbours: Int) {
         // given
         val tested = Cube(false)
