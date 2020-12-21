adf3c7fe02f36fc3e174cc1f81c427c08404da21
Łukasz Marek <marek.lucas@gmail.com>
2020-12-21 22:49:40 +0100

    day21 SmartRecursiveSolver in progress

--- src/main/kotlin/day21/Allergens.kt
+++ src/main/kotlin/day21/Allergens.kt
@@ -7,10 +7,14 @@ inline class Ingredient(val name: String)
 data class Food(val ingredients: Set<Ingredient>, val allergens: Set<Allergen>)
 
 private val foodPattern = Regex("^(.+) \\(contains (.+)\\)$")
-fun parseFood(food: String): Food{
+fun parseFood(food: String): Food {
     val (ingredientsPart, allergensPart) = foodPattern.matchEntire(food)!!.destructured
-    val ingredients = ingredientsPart.split(" ").asSequence().filter { it.isNotEmpty() }.map { it.trim() }.map { Ingredient(it) }.toSet()
-    val allergens = allergensPart.split(",").asSequence().filter { it.isNotEmpty() }.map { it.trim() }.map { ConcreteAllergen(it) }.toSet()
+    val ingredients =
+        ingredientsPart.split(" ").asSequence().filter { it.isNotEmpty() }.map { it.trim() }.map { Ingredient(it) }
+            .toSet()
+    val allergens =
+        allergensPart.split(",").asSequence().filter { it.isNotEmpty() }.map { it.trim() }.map { ConcreteAllergen(it) }
+            .toSet()
     return Food(ingredients, allergens)
 }
 
@@ -24,24 +28,26 @@ class SimpleRecursiveSolver : Solver {
     override fun solve(foods: List<Food>): Map<Ingredient, Allergen>? {
         val allIngredients = foods.flatMap { it.ingredients }.toSet()
         val allAllergens = foods.flatMap { it.allergens }.toSet()
-        return solve(foods,allIngredients, allAllergens, emptyMap())
+        return solve(foods, allIngredients, allAllergens, emptyMap())
     }
 
-    private fun solve(foods: List<Food>,
-                      ingredients: Set<Ingredient>,
-                      allergens: Set<Allergen>,
-                      solution: Map<Ingredient,Allergen>): Map<Ingredient, Allergen>? {
-        if(ingredients.isEmpty()){
+    private fun solve(
+        foods: List<Food>,
+        ingredients: Set<Ingredient>,
+        allergens: Set<Allergen>,
+        solution: Map<Ingredient, Allergen>
+    ): Map<Ingredient, Allergen>? {
+        if (ingredients.isEmpty()) {
             return solution
         }
         val ingredient = ingredients.first()
         val remainingIngredients = ingredients.drop(1).toSet()
-        for(allergen in allergens + EmptyAllergen){
+        for (allergen in allergens + EmptyAllergen) {
             val currentSolution = solution + (ingredient to allergen)
             val noConflicts = foods.all { it.isSatisfiable(currentSolution) }
-            if(noConflicts){
+            if (noConflicts) {
                 val maybeSolution = solve(foods, remainingIngredients, allergens - allergen, currentSolution)
-                if(maybeSolution != null){
+                if (maybeSolution != null) {
                     return maybeSolution
                 }
             }
@@ -54,5 +60,51 @@ class SimpleRecursiveSolver : Solver {
         val detectedAllergens = this.ingredients.mapNotNull { mapping[it] }
         return detectedAllergens.containsAll(expectedAllergens) || detectedAllergens.size < ingredients.size
     }
+}
+
+class SmartRecursiveSolver : Solver {
+    override fun solve(foods: List<Food>): Map<Ingredient, Allergen>? {
+        val ingredients = foods.asSequence().flatMap { it.ingredients }.distinct()
+            .sortedBy { ingredient -> foods.count { it.ingredients.contains(ingredient) } }.toList()
+        val allergens = foods.flatMap { it.allergens }.toSet()
+        val foodsByIngredient = ingredients.map { it to foods.filter { food -> food.ingredients.contains(it) } }.toMap()
+        return solve(foodsByIngredient, ingredients, allergens, emptyMap())
+    }
+
+    private fun solve(
+        foodsByIngredient: Map<Ingredient, List<Food>>,
+        ingredients: List<Ingredient>,
+        allergens: Set<Allergen>,
+        solution: Map<Ingredient, Allergen>
+    ): Map<Ingredient, Allergen>? {
+        if (ingredients.isEmpty()) {
+            return solution
+        }
+
+        val ingredient = ingredients.first()
+        val remainingIngredients = ingredients.drop(1)
+        val foods = foodsByIngredient[ingredient]!!
+
+        for (allergen in allergens + EmptyAllergen) {
+            val currentSolution = solution + (ingredient to allergen)
+            val noConflicts = foods.all { it.isSatisfiable(currentSolution) }
+            if (noConflicts) {
+                val maybeSolution =
+                    solve(foodsByIngredient, remainingIngredients, allergens - allergen, currentSolution)
+                if (maybeSolution != null) {
+                    return maybeSolution
+                }
+            }
+        }
+        return null
+    }
+
+    private fun Food.isSatisfiable(mapping: Map<Ingredient, Allergen>): Boolean {
+        val expectedAllergens = this.allergens
+        val detectedAllergens = this.ingredients.mapNotNull { mapping[it] }
+        return detectedAllergens.containsAll(expectedAllergens) || detectedAllergens.size < ingredients.size
+    }
+
+//    private fun findConflictingAssignments(foods: List<Food>, solution: Map<Ingredient, Allergen>): Set<>
 }
 
