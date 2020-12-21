e1db69cbc91ff6ae2d1a0ba1c615d759074545df
Łukasz Marek <marek.lucas@gmail.com>
2020-12-21 21:31:31 +0100

    formatting

--- src/test/kotlin/day21/SimpleRecursiveSolverTest.kt
+++ src/test/kotlin/day21/SimpleRecursiveSolverTest.kt
@@ -10,19 +10,32 @@ class SimpleRecursiveSolverTest {
     private val tested = SimpleRecursiveSolver()
 
     @Test
-    fun `should return valid results for simple problem`(){
+    fun `should return valid results for simple problem`() {
 //        mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
 //        trh fvjkl sbzzf mxmxvkd (contains dairy)
 //        sqjhc fvjkl (contains soy)
 //        sqjhc mxmxvkd sbzzf (contains fish)
         // given
         val foods = listOf(
-            Food(setOf("mxmxvkd","kfcds","sqjhc","nhms").map { Ingredient(it) }.toSet(), setOf("dairy", "fish").map { ConcreteAllergen(it) }.toSet()),
-            Food(setOf("trh","fvjkl","sbzzf","mxmxvkd").map { Ingredient(it) }.toSet(), setOf("dairy").map { ConcreteAllergen(it) }.toSet()),
-            Food(setOf("sqjhc","fvjkl").map { Ingredient(it) }.toSet(), setOf("soy").map { ConcreteAllergen(it) }.toSet()),
-            Food(setOf("sqjhc","mxmxvkd","sbzzf").map { Ingredient(it) }.toSet(), setOf("fish").map { ConcreteAllergen(it) }.toSet()))
-       // when
-       val solution = tested.solve(foods)
+            Food(
+                setOf("mxmxvkd", "kfcds", "sqjhc", "nhms").map { Ingredient(it) }.toSet(),
+                setOf("dairy", "fish").map { ConcreteAllergen(it) }.toSet()
+            ),
+            Food(
+                setOf("trh", "fvjkl", "sbzzf", "mxmxvkd").map { Ingredient(it) }.toSet(),
+                setOf("dairy").map { ConcreteAllergen(it) }.toSet()
+            ),
+            Food(
+                setOf("sqjhc", "fvjkl").map { Ingredient(it) }.toSet(),
+                setOf("soy").map { ConcreteAllergen(it) }.toSet()
+            ),
+            Food(
+                setOf("sqjhc", "mxmxvkd", "sbzzf").map { Ingredient(it) }.toSet(),
+                setOf("fish").map { ConcreteAllergen(it) }.toSet()
+            )
+        )
+        // when
+        val solution = tested.solve(foods)
         // then
         expectThat(solution).isNotNull().and {
 //            kfcds, nhms, sbzzf, or trh
