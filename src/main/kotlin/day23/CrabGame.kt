cf3e059e8936f73297c71c4c3abbd1b1b4df0538
Łukasz Marek <marek.lucas@gmail.com>
2020-12-24 21:29:04 +0100

    day23 optimization part4

--- src/main/kotlin/day23/CrabGame.kt
+++ src/main/kotlin/day23/CrabGame.kt
@@ -11,19 +11,20 @@ fun play(input: List<Cup>, moves: Int): List<Cup> {
         val takenCupsIndexes = (1..3).asSequence()
             .map { it + currentCupIndex }
             .map { it % cups.size }
+            .toList()
 
-        val cupsTaken = takenCupsIndexes.withIndex()
+        val cupsTaken = takenCupsIndexes.asSequence()
+            .withIndex()
             .sortedByDescending { it.value }
             .map { it.index to cups.removeAt(it.value) }
             .sortedBy { it.first }
             .map { it.second }.toList()
 
-        val destinationCup = generateSequence(currentCup.value - 1) { it - 1 }
+        val destinationCupIndex = generateSequence(currentCup.value - 1) { it - 1 }
             .takeWhile { it >= 0 }
-            .firstOrNull { cups.any { cup -> cup.value == it } }
-            ?.let { Cup(it) }
-            ?: cups.maxByOrNull { it.value }!!
-        val destinationCupIndex = cups.indexOf(destinationCup)
+            .flatMap { cups.asSequence().withIndex().filter { (_, value) -> value.value == it }.map { it.index } }
+            .firstOrNull()
+            ?: cups.withIndex().maxByOrNull { it.value.value }!!.index
         cups.addAll(destinationCupIndex + 1, cupsTaken)
 
         currentCupIndex = (cups.indexOf(currentCup) + 1) % cups.size
